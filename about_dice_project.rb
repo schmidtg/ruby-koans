3ac87a0fc72c68f60e3f4cb31e2ed05e1fa0a74b
Graham Schmidt <graham.schmidt@worldone.com>
2013-06-24 09:22:19 -0400

    Finish about dice project testing.

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+
+  def initialize
+    @values = (1..6).to_a
+  end
+
+  def roll(di)
+    @values = @values.shuffle.first(di)
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
